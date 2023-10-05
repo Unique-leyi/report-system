0ec4f3ed85b340cfb1f2539660cc875611b2b1fc
Ayoleyi <ayoleyi@macs-MacBook-Pro.local>
2023-10-05 14:59:15 +0100

    feat: report-system setup

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        black: "#000",
+        primary: "#141F1F",
+        secondary: "#0e1515",
+        farblue: "#7DF9FF",
+        farash: "#A5B3B3",
+        farlightash: "#7F9192",
+      }
+  },
+
+    fontFamily: {
+      opensauce: ["Open Sauce One", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+      outfit: ["Outfit", "sans-serif"],
+    }
+  },
+  plugins: [],
+}
+
