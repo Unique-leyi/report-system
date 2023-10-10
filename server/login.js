bdb901b8065dd428ee53ad4ccb683b3a9811e381
Ayoleyi <ayoleyi@macs-MacBook-Pro.local>
2023-10-10 15:38:09 +0100

    add: added changes

--- server/login.js
+++ server/login.js
@@ -1,23 +1,23 @@
-import { readJson } from 'fs-extra'
+// import { readJson } from 'fs-extra'
 
-export const login = async (username, password) => {
+// export const login = async (username, password) => {
 
-  const users = JSON.parse(await readJson('./users.json'));
+//   const users = JSON.parse(await readJson('./users.json'));
 
-  // Find the user by username or email
-  const user = users.find(
-    (u) =>
-      u.username === username
-  );
+//   // Find the user by username or email
+//   const user = users.find(
+//     (u) =>
+//       u.username === username
+//   );
 
-  if (!user) {
-    return null; 
-  }
+//   if (!user) {
+//     return null; 
+//   }
 
-  // Check if the entered password matches the hashed password
-  if (password === user.password) {
-    return user; 
-  } else {
-    return null; 
-  }
-};
+//   // Check if the entered password matches the hashed password
+//   if (password === user.password) {
+//     return user; 
+//   } else {
+//     return null; 
+//   }
+// };
--- server/reportCrud.js
+++ server/reportCrud.js
@@ -1,92 +1,92 @@
-import { readJson, writeJson } from 'fs-extra'
+// import { readJson, writeJson } from 'fs-extra'
 
 
-async function getReports() {
-  const data = await readJson('./reports.json');
-  return JSON.parse(data);
-}
+// async function getReports() {
+//   const data = await readJson('./reports.json');
+//   return JSON.parse(data);
+// }
 
 
-function generateUniqueId() {
-    const timestamp = new Date().getTime(); 
-    const randomNum = Math.floor(Math.random() * 1000);
+// function generateUniqueId() {
+//     const timestamp = new Date().getTime(); 
+//     const randomNum = Math.floor(Math.random() * 1000);
   
-    const uniqueId = timestamp * 1000 + randomNum;
+//     const uniqueId = timestamp * 1000 + randomNum;
   
-    return uniqueId;
-  }
+//     return uniqueId;
+//   }
 
-async function writeReports(Reports) {
-  await writeJson('./reports.json', JSON.stringify(Reports, null, 2));
-}
+// async function writeReports(Reports) {
+//   await writeJson('./reports.json', JSON.stringify(Reports, null, 2));
+// }
 
-// Create a new Report
-export async function createReport(title, tags, images, date, author) {
+// // Create a new Report
+// export async function createReport(title, tags, images, date, author) {
 
-     const id = generateUniqueId();
+//      const id = generateUniqueId();
 
-     const newReport = {
-        id, 
-        task_title: title,
-        task_tags: [...tags],
-        task_images: [...images],
-        task_date: date,
-        task_author: author,
-    }
+//      const newReport = {
+//         id, 
+//         task_title: title,
+//         task_tags: [...tags],
+//         task_images: [...images],
+//         task_date: date,
+//         task_author: author,
+//     }
   
 
-  const Reports = await getReports();
-  Reports.push(newReport);
-  await writeReports(Reports);
+//   const Reports = await getReports();
+//   Reports.push(newReport);
+//   await writeReports(Reports);
 
-  return newReport;
-}
+//   return newReport;
+// }
 
-// Find a Report
-export async function getReport(ReportnameOrEmail) {
-  const Reports = await getReports();
-  return Reports.find(
-    (Report) =>
-      Report.Reportname === ReportnameOrEmail || Report.email === ReportnameOrEmail
-  );
-}
+// // Find a Report
+// export async function getReport(ReportnameOrEmail) {
+//   const Reports = await getReports();
+//   return Reports.find(
+//     (Report) =>
+//       Report.Reportname === ReportnameOrEmail || Report.email === ReportnameOrEmail
+//   );
+// }
 
-// Update Report
-export async function updateReport(ReportnameOrEmail, updatedReportData) {
-  const Reports = await getReports();
-  const ReportIndex = Reports.findIndex(
-    (Report) =>
-      Report.Reportname === ReportnameOrEmail || Report.email === ReportnameOrEmail
-  );
+// // Update Report
+// export async function updateReport(ReportnameOrEmail, updatedReportData) {
+//   const Reports = await getReports();
+//   const ReportIndex = Reports.findIndex(
+//     (Report) =>
+//       Report.Reportname === ReportnameOrEmail || Report.email === ReportnameOrEmail
+//   );
 
-  if (ReportIndex === -1) {
-    return null; 
-  }
+//   if (ReportIndex === -1) {
+//     return null; 
+//   }
 
-  Reports[ReportIndex] = {
-    ...Reports[ReportIndex],
-    ...updatedReportData,
-  };
+//   Reports[ReportIndex] = {
+//     ...Reports[ReportIndex],
+//     ...updatedReportData,
+//   };
 
-  await writeReports(Reports);
+//   await writeReports(Reports);
 
-  return Reports[ReportIndex];
-}
+//   return Reports[ReportIndex];
+// }
 
-// Delete Report 
-export async function deleteReport(ReportnameOrEmail) {
-  const Reports = await getReports();
-  const ReportIndex = Reports.findIndex(
-    (Report) =>
-      Report.Reportname === ReportnameOrEmail || Report.email === ReportnameOrEmail
-  );
+// // Delete Report 
+// export async function deleteReport(ReportnameOrEmail) {
+//   const Reports = await getReports();
+//   const ReportIndex = Reports.findIndex(
+//     (Report) =>
+//       Report.Reportname === ReportnameOrEmail || Report.email === ReportnameOrEmail
+//   );
 
-  if (ReportIndex === -1) {
-    return false; // Report not found
-  }
+//   if (ReportIndex === -1) {
+//     return false; // Report not found
+//   }
 
-  Reports.splice(ReportIndex, 1);
-  await writeReports(Reports);
+//   Reports.splice(ReportIndex, 1);
+//   await writeReports(Reports);
 
-  return true; 
-}
+//   return true; 
+// }
